670d35d47211402c9e9f5c81bb2462493d98ce1c
FelipeMel0 <felipedemfernandes@gmail.com>
2021-11-19 09:04:06 -0300

    Primeiro commit

---
+++ controller/ControllerPessoa.php
@@ -0,0 +1,42 @@
+<?php
+
+class Controller
+{
+
+    private $_method;
+    private $_modelPessoa;
+
+    public function __construct($model)
+    {
+        $this->_modelPessoa = $model;
+        $this->_method = $_SERVER['REQUEST_METHOD'];
+    }
+
+    function router()
+    {
+
+        switch ($this->_method) {
+            case 'GET':
+
+                return $this->_modelPessoa->findAll();
+                
+                break;
+
+            case 'POST':
+                # code...
+                break;
+
+            case 'PUT':
+                # code...
+                break;
+
+            case 'DELETE':
+                # code...
+                break;
+
+            default:
+                # code...
+                break;
+        }
+    }
+}
